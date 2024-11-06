741675f86a6dade0c7986b9f080b17e80169908b
cheldieva-l <68663613+cheldieva-l@users.noreply.github.com>
2024-11-06 03:23:10 +0300

    Update model.py

--- pilgrim/model.py
+++ pilgrim/model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class ResidualBlock(nn.Module):
+class ResidualBlock_old(nn.Module):
     def __init__(self, hidden_dim, dropout_rate=0.1, activation_function="mish", use_batch_norm=True):#relu
         super(ResidualBlock, self).__init__()
         self.fc1 = nn.Linear(hidden_dim, hidden_dim)
@@ -35,6 +35,85 @@ class ResidualBlock(nn.Module):
             return nn.Mish()
         else:
             raise ValueError(f"Unknown activation function: {name}")
+
+
+%%time
+import torch.optim as optim
+import torch.nn.functional as F
+# from torcheval.metrics import R2Score, MeanSquaredError
+
+input_layer_size_for_one_hot = state_size * n_unique_symbols_in_states
+
+# layer_1_size =1165
+layer_1_size =  CFG['list_layers_sizes'][0]
+
+#  Model definition
+class ImprovedNet(nn.Module): 
+    def __init__(self, input_size, n_unique_symbols_in_states): 
+        super(ImprovedNet, self).__init__() 
+        
+        self.n_unique_symbols_in_states = n_unique_symbols_in_states # More gradual compression 
+        
+        self.layers = nn.Sequential( 
+            nn.Linear(input_size, 2048), 
+            nn.BatchNorm1d(2048), 
+            nn.Mish(), 
+            
+            nn.Linear(2048, 1024), 
+            nn.BatchNorm1d(1024), 
+            nn.Mish(), 
+            
+            nn.Linear(1024, 512), 
+            nn.BatchNorm1d(512), 
+            nn.Mish(), 
+            
+            nn.Linear(512, 256), 
+            nn.BatchNorm1d(256), 
+            nn.Mish(), 
+            
+            nn.Linear(256, 128), 
+            nn.BatchNorm1d(128), 
+            nn.Mish(), 
+            
+            nn.Linear(128, 1) 
+        ) 
+        
+        # Skip connections 
+        self.skip1 = nn.Linear(2048, 1024) 
+        self.skip2 = nn.Linear(1024, 512) 
+        self.skip3 = nn.Linear(512, 256) 
+        
+    def forward(self, x): 
+        x = torch.nn.functional.one_hot(x.long(), num_classes=self.n_unique_symbols_in_states).float() 
+        x = x.view(x.size(0), -1) # Add skip connections 
+        x1 = self.layers[0:3](x) # 2048 
+        x2 = self.layers[3:6](x1) + self.skip1(x1) # 1024 
+        x3 = self.layers[6:9](x2) + self.skip2(x2) # 512 
+        x4 = self.layers[9:12](x3) + self.skip3(x3) # 256 
+        out = self.layers[12:](x4) # Final layers return out
+        return out
+
+
+# Usage
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)    
+input_layer_size_for_one_hot = state_size * n_unique_symbols_in_states
+model = ImprovedNet(input_size=input_layer_size_for_one_hot, n_unique_symbols_in_states=n_unique_symbols_in_states).to(device)
+try:
+    #model.load_state_dict(torch.load(checkpoint_path,weights_only = False))
+    #model.eval()  
+    #print('Weights are loaded')
+    print(f'Weights are not loaded.')
+    pass
+except Exception as error:
+    print(f'Weights are not loaded. Error: {error}')
+    raise RuntimeError(f'Weights are not loaded. Error: {error}')
+    
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters())#, lr=0.0005)
+
+
+
 
 class Pilgrim(nn.Module):
     def __init__(self, state_size, hd1=5000, hd2=1000, nrd=2, output_dim=1, dropout_rate=0.1, activation_function="mish", use_batch_norm=True): #relu
