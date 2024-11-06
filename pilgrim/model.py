d43f02f8523c3566fcdb152dd624856463867086
cheldieva-l <68663613+cheldieva-l@users.noreply.github.com>
2024-11-06 03:50:03 +0300

    Update model.py
    1

--- pilgrim/model.py
+++ pilgrim/model.py
@@ -28,7 +28,7 @@ class ResidualBlock_old(nn.Module):
         return out
 
     @staticmethod
-    def _get_activation_function(name):
+    def _get_activation_function(name):  # +
         if name == "relu":
             return nn.ReLU()
         elif name == "mish":
@@ -47,15 +47,15 @@ input_layer_size_for_one_hot = state_size * n_unique_symbols_in_states
 # layer_1_size =1165
 layer_1_size =  CFG['list_layers_sizes'][0]
 
-#  Model definition
-class ImprovedNet(nn.Module): 
-    def __init__(self, input_size, n_unique_symbols_in_states): 
-        super(ImprovedNet, self).__init__() 
+ 
+class Pilgrim(nn.Module):
+    def __init__(self, state_size, hd1=5000, hd2=1000, nrd=2, output_dim=1, dropout_rate=0.1, activation_function="mish", use_batch_norm=True): #relu
+          super(Pilgrim, self).__init__() 
         
         self.n_unique_symbols_in_states = n_unique_symbols_in_states # More gradual compression 
         
         self.layers = nn.Sequential( 
-            nn.Linear(input_size, 2048), 
+            nn.Linear(state_size * 6, 2048), 
             nn.BatchNorm1d(2048), 
             nn.Mish(), 
             
@@ -93,79 +93,6 @@ class ImprovedNet(nn.Module):
         out = self.layers[12:](x4) # Final layers return out
         return out
 
-
-# Usage
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print(device)    
-input_layer_size_for_one_hot = state_size * n_unique_symbols_in_states
-model = ImprovedNet(input_size=input_layer_size_for_one_hot, n_unique_symbols_in_states=n_unique_symbols_in_states).to(device)
-try:
-    #model.load_state_dict(torch.load(checkpoint_path,weights_only = False))
-    #model.eval()  
-    #print('Weights are loaded')
-    print(f'Weights are not loaded.')
-    pass
-except Exception as error:
-    print(f'Weights are not loaded. Error: {error}')
-    raise RuntimeError(f'Weights are not loaded. Error: {error}')
-    
-criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters())#, lr=0.0005)
-
-
-
-
-class Pilgrim(nn.Module):
-    def __init__(self, state_size, hd1=5000, hd2=1000, nrd=2, output_dim=1, dropout_rate=0.1, activation_function="mish", use_batch_norm=True): #relu
-        super(Pilgrim, self).__init__()
-        self.hd1 = hd1
-        self.hd2 = hd2
-        self.nrd = nrd
-        self.use_batch_norm = use_batch_norm
-
-        input_dim = state_size * 6
-        self.input_layer = nn.Linear(input_dim, hd1)
-        self.bn1 = nn.BatchNorm1d(hd1) if use_batch_norm else None
-        self.activation = self._get_activation_function(activation_function)
-        self.dropout = nn.Dropout(dropout_rate)
-
-        if hd2 > 0:
-            self.hidden_layer = nn.Linear(hd1, hd2)
-            self.bn2 = nn.BatchNorm1d(hd2) if use_batch_norm else None
-            hidden_dim_for_output = hd2
-        else:
-            self.hidden_layer = None
-            self.bn2 = None
-            hidden_dim_for_output = hd1
-
-        if nrd > 0 and hd2 > 0:
-            self.residual_blocks = nn.ModuleList([ResidualBlock(hd2, dropout_rate, activation_function, use_batch_norm) for _ in range(nrd)])
-        else:
-            self.residual_blocks = None
-
-        self.output_layer = nn.Linear(hidden_dim_for_output, output_dim)
-
-    def forward(self, z):
-        x = F.one_hot(z.long(), num_classes=6).float().view(z.size(0), -1)
-        x = self.input_layer(x)
-        if self.use_batch_norm:
-            x = self.bn1(x)
-        x = self.activation(x)
-        x = self.dropout(x)
-
-        if self.hidden_layer:
-            x = self.hidden_layer(x)
-            if self.bn2:
-                x = self.bn2(x)
-            x = self.activation(x)
-            x = self.dropout(x)
-
-        if self.residual_blocks:
-            for block in self.residual_blocks:
-                x = block(x)
-
-        x = self.output_layer(x)
-        return x.flatten()
 
     @staticmethod
     def _get_activation_function(name):
